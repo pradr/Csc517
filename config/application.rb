7e2165e042ea9eadbf2bb04ef3c187b952eed1a0
Pradeep <praad.kumar@gmail.com>
2011-10-08 01:10:07 -0400

    6

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ if defined?(Bundler)
   Bundler.require(:default, :assets, Rails.env) if defined?(Bundler)
 
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Project1
